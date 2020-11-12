73ea20abba8a4aa8ef7e6bfff0d8e7d932b68b77
Laurenz <laurenz.dilba@hotmail.de>
2020-11-12 15:43:42 +0100

    Added contains function

--- src/org/hbrs/se/ws2020/uebung2/Container.java
+++ src/org/hbrs/se/ws2020/uebung2/Container.java
@@ -3,21 +3,36 @@ package org.hbrs.se.ws2020.uebung2;
 import java.util.LinkedList;
 
 public class Container {
-    LinkedList list = new LinkedList<Member>();
+    LinkedList<Member> list = new LinkedList<>();
 
-    public void addMember(Member member){
 
+    public void addMember(Member member) {
+
+        if (!contains(member.getID())) {
+            list.add(member);
+        } else {
+
+        }
     }
 
-    public String deleteMember(int id){
+    private boolean contains(int id) {
+        for(Member m : list){
+            if (m.getID().equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String deleteMember(int id) {
         return "";
     }
 
-    public void dump(){
+    public void dump() {
 
     }
 
-    public int size(){
+    public int size() {
         return list.size();
     }
 }
