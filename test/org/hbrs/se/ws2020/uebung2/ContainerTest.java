418822ecb3a2111e2499b21ae9450451c0f46f0f
Laurenz <laurenz.dilba@hotmail.de>
2020-11-12 16:07:19 +0100

    Added Testclass

---
+++ test/org/hbrs/se/ws2020/uebung2/ContainerTest.java
@@ -0,0 +1,32 @@
+package org.hbrs.se.ws2020.uebung2;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addMember() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+
+    @Test
+    void dump() {
+    }
+
+    @Test
+    void size() {
+    }
+}
