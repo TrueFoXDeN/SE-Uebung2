e9615dea757beedf5e10efabfb1011571c68cc88
Laurenz <laurenz.dilba@hotmail.de>
2020-11-12 15:18:11 +0100

    Added functions

--- src/org/hbrs/se/ws2020/uebung2/Container.java
+++ src/org/hbrs/se/ws2020/uebung2/Container.java
@@ -4,4 +4,20 @@ import java.util.LinkedList;
 
 public class Container {
     LinkedList list = new LinkedList<Member>();
+
+    public void addMember(Member member){
+
+    }
+
+    public String deleteMember(int id){
+        return "";
+    }
+
+    public void dump(){
+
+    }
+
+    public int size(){
+        return list.size();
+    }
 }
